5eec315f2f3c0f3ec2304700db7bfd1821b6731a
ErykZenith <167299127+ErykZenith@users.noreply.github.com>
2024-09-12 08:42:40 +0700

    remove

--- Client.lua
+++ Client.lua
@@ -9,16 +9,13 @@ function requestParticles(name)
 	end
 end
 
-function particles_In_Entity(entity, xOffset, yOffset, zOffset, xRot, yRot, zRot, scale)
+function particles_In_Entity(entity, xOffset, yOffset, zOffset, xRot, yRot, zRot, r, g, g scale)
 	requestParticles("eryk_particle_addon")
 	if not entity or not DoesEntityExist(entity) then return end
 	UseParticleFxAsset("eryk_particle_addon")
 	local handle = StartNetworkedParticleFxLoopedOnEntity("eryk_effect_sparks", entity, xOffset, yOffset, zOffset, xRot, yRot, zRot, scale, 1, 1, 1)
-	SetParticleFxLoopedEvolution(handle, "speed", 1.0, true)
-	SetParticleFxLoopedColour(handle, 1.0, 0.0, 0.0, 0)
-	SetParticleFxLoopedAlpha(handle, 100.0)
+	SetParticleFxLoopedColour(handle, r, g, g, 0)
 	return handle
 end
 
-
-particles_In_Entity(PlayerPedId(),0.0, 0.0, 0.0, 0.0, 90.0, 0.0, 2.0)
+particles_In_Entity(PlayerPedId(),0.0, 0.0, 0.0, 0.0, 90.0, 0.0, 1.0, 0.0, 0.0 2.0)
